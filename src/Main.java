5a7f6b8414f34dd622017d3d99f07bfb44aca83e
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-23 18:50:01 +0500

    Java variables

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,44 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // This is my first java program
+        // PRIMITIVE DATA TYPES
+        int age = 21;
+        int year = 2026;
+        int quantity = 1;
 
-        /*
-         * This
-         * is
-         * a
-         * multi-line
-         * comment
-         * */
+        double price = 90.98;
+        double gpa = 4.65;
+        double temp = -12.5;
 
-        System.out.println("I like pizza!");
-        System.out.println("It's really good!");
-        System.out.println("Buy me pizza!");
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
 
+        boolean isStudent = true;
+        boolean forSale = true;
+
+        if (isStudent) {
+//            System.out.println("You are a student");
+        } else {
+//            System.out.println("You are not a student");
+        }
+
+        // REFERENCE DATA TYPES
+        String name = "Asilbek";
+        String food = "pizza";
+        String email = "fake1234@gmail.com";
+        String color = "red";
+        String car = "bmw";
+
+        System.out.println("Your choice is a " + color + " " + year + " " + car);
+        System.out.println("The price is " + currency + price);
+
+        if (forSale) {
+            System.out.println("There is a " + car + " for sale.");
+        } else {
+            System.out.println("The " + car + " is not for sale.");
+
+        }
     }
 
 }
