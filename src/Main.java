41219ce160f9c4678a9705ea492bab1a21dd5fbc
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-23 18:18:35 +0500

    first lesson: comment and sout explanation

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // This is my first java program
+
+        /*
+         * This
+         * is
+         * a
+         * multi-line
+         * comment
+         * */
+
+        System.out.println("I like pizza!");
+        System.out.println("It's really good!");
+        System.out.println("Buy me pizza!");
+
+    }
+
+}
