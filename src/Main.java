2c9f10fde9cdeb2a1f310cfc7f4173d899952086
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-24 22:07:10 +0500

    User input creation

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,72 @@
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
 
-        // PRIMITIVE DATA TYPES
-        int age = 21;
-        int year = 2026;
-        int quantity = 1;
+       /* Scanner scanner = new Scanner(System.in);
 
-        double price = 90.98;
-        double gpa = 4.65;
-        double temp = -12.5;
+        System.out.print("Enter your name: ");
 
-        char grade = 'A';
-        char symbol = '!';
-        char currency = '$';
+        String name = scanner.nextLine();
+//        String name = scanner.next(); --> next() doesn't read any spaces
 
-        boolean isStudent = true;
-        boolean forSale = true;
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Are you a student? (true/false): ");
+        boolean isStudent = scanner.nextBoolean();
+
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old!");
 
         if (isStudent) {
-//            System.out.println("You are a student");
+            System.out.println("You are a student");
+
+            System.out.print("What is your gpa: ");
+            double gpa = scanner.nextDouble();
+
+            if (gpa > 3.0) {
+                System.out.println("Your gpa is good: " + gpa);
+            } else {
+                System.out.println("Your gpa is low: " + gpa);
+            }
+
         } else {
-//            System.out.println("You are not a student");
+            System.out.println("You are not a student");
         }
 
-        // REFERENCE DATA TYPES
-        String name = "Asilbek";
-        String food = "pizza";
-        String email = "fake1234@gmail.com";
-        String color = "red";
-        String car = "bmw";
+        // COMMON ISSUES
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("Your choice is a " + color + " " + year + " " + car);
-        System.out.println("The price is " + currency + price);
+        System.out.print("Enter your favourite color: ");
+        String color = scanner.nextLine();
 
-        if (forSale) {
-            System.out.println("There is a " + car + " for sale.");
-        } else {
-            System.out.println("The " + car + " is not for sale.");
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like the color " + color);
 
-        }
+        scanner.close();*/
+
+        // Calculate area of a rectangle
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the height: ");
+        height = scanner.nextDouble();
+
+        area = width * height;
+
+        System.out.println("The rectanle are is: " + area + "cm²");
+
+        scanner.close();
+
     }
-
 }
