91dad5e2f232338a91f0540525acdc9dacab3712
Asilbek <asilbekkaromatov2@gmail.com>
2026-09-26 21:48:24 +0500

    Java arithmetic is easy! 🧮

---
+++ arithmetics/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+//        int z;
+
+//        z = x - y;
+//        z = x + y;
+//        z = x * y;
+//        z = x % y;
+
+
+        // Augmented Assignment Operators
+
+//        x += y;
+        x -= y;
+
+        System.out.println(x);
+
+
+    }
+}
